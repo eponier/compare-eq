979cb46a4a4de2847de61ae278a9e40494ec7e86
Jean-Christophe Léchenet <jean-christophe.lechenet@inria.fr>
2022-07-19 16:27:34 +0200

    Equations: generate all at once

--- perfs/triangle_10/equations.v
+++ perfs/triangle_10/equations.v
@@ -3,7 +3,6 @@ From Equations Require Import Equations.
 Require Import type.
 
 (* Derive NoConfusionHom seems much slower, so we use Derive NoConfusion *)
-Time Equations Derive NoConfusion for t.
 (* Time Equations Derive NoConfusionHom for t. *)
 
-Time Equations Derive EqDec for t.
+Time Equations Derive NoConfusion EqDec for t.
