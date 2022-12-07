3c42435cf47e4aba29ad1ee21b6cd92cd0b35641
Jean-Christophe Léchenet <jean-christophe.lechenet@inria.fr>
2022-12-07 20:13:38 +0100

    always return bool

--- equality_test/14/equations.v
+++ equality_test/14/equations.v
@@ -4,6 +4,6 @@ Require Import type.
 
 Derive NoConfusion EqDec for tree.
 
-Eval vm_compute in tree_eqdec t0 t1.
-Time Eval vm_compute in tree_eqdec t0 t0.
-Time Eval vm_compute in tree_eqdec t0 t1.
+Eval vm_compute in if tree_eqdec t0 t1 then true else false.
+Time Eval vm_compute in if tree_eqdec t0 t0 then true else false.
+Time Eval vm_compute in if tree_eqdec t0 t1 then true else false.
--- equality_test/8/decide_equality.v
+++ equality_test/8/decide_equality.v
@@ -3,6 +3,6 @@ Require Import type.
 Definition dec_eq : forall (x y : tree), {x=y} + {x<>y}.
 Proof. decide equality. decide equality. Defined.
 
-Eval compute in dec_eq t0 t1.
-Time Eval compute in dec_eq t0 t0.
-Time Eval compute in dec_eq t0 t1.
+Eval compute in if dec_eq t0 t1 then true else false.
+Time Eval compute in if dec_eq t0 t0 then true else false.
+Time Eval compute in if dec_eq t0 t1 then true else false.
